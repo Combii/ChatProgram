c5edc1bb4bb7e464a9cde731e609660dcaddb42f
Combii <davidstovle@gmail.com>
2017-02-20 19:42:03 +0100

    Update

---
+++ src/Client.java
@@ -0,0 +1,24 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by David Stovlbaek
+ * 16 February 2017.
+ */
+public class Client {
+
+    public static void sendText(int port, String ip, String message) {
+    try {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress netIp = InetAddress.getByName(ip);
+
+        DatagramPacket dp = new DatagramPacket(message.getBytes(), message.length(), netIp, port);
+        socket.send(dp);
+        System.out.println("Port: "+ port + " \nIp: " + ip + "\nSent!");
+    } catch (Exception e){
+        e.printStackTrace();
+    }
+    }
+
+}
