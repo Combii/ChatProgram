79ec52fad2d17e925387ca0403fdaa5c3125cec6
Combii <davidstovle@gmail.com>
2017-02-22 16:10:30 +0100

    Fixed Client

--- src/Client/Tester.java
+++ src/Client/Tester.java
@@ -1,6 +1,5 @@
 package Client;
 
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -11,16 +10,12 @@ import java.net.UnknownHostException;
 public class Tester {
     public static void main(String[] args) throws UnknownHostException {
         try {
-            Client client = new Client("Hello", "172.20.10.4", 1234);
+            Client client = new Client("David", "10.0.1.38", 1234);
             client.sendText("Hej");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (SocketException e) {
             e.printStackTrace();
         }
-
-
-        InetAddress ownIP=InetAddress.getLocalHost();
-        System.out.println("IP of my system is := "+ownIP.getAddress().toString());
     }
 }
--- src/Client/TextWindowController1.java
+++ src/Client/TextWindowController1.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
@@ -15,9 +16,9 @@ public class TextWindowController1 {
     public Text username;
 
     @FXML
-    public void initialize() throws UnknownHostException {
+    public void initialize() throws UnknownHostException, SocketException {
         username.setText(EnterUsernameController.staticUsername);
-       // Client user = new Client()
+       Client user = new Client(username.getText(), "10.0.1.38", 1234);
     }
 
     public void sendButton(ActionEvent actionEvent) {
