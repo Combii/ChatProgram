c2be4f9f4e98a1eb6213525802427036a8c67757
Combii <davidstovle@gmail.com>
2017-02-28 11:00:27 +0100

    Clean up

--- src/Client/RunClient.java
+++ src/Client/RunClient.java
@@ -8,7 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
